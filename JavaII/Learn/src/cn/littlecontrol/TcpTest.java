2419ce8c79217c317f3cdb6da66e18979f37126f
LittleControl <i@littlecontrol.cc>
2020-05-13 22:40:00 +0800

    Learn初始化

---
+++ JavaII/Learn/src/cn/littlecontrol/TcpTest.java
@@ -0,0 +1,15 @@
+package cn.littlecontrol;
+
+
+import org.junit.Test;
+
+/**
+ * @Author: www.littlecontrol.cn
+ * @Date: 2020/5/12 上午 09:11
+ */
+public class TcpTest {
+    @Test
+    public void show() {
+        System.out.println("show1");
+    }
+}
